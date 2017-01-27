03b8c46d8b0a7ec6b3fe5e3c417aba4e7892874f
10395803 <stefano.ubbiali@mail.polimi.it>
2017-01-27 13:15:04 +0100

    Small changes.

--- main/main_simplification.cpp
+++ main/main_simplification.cpp
@@ -11,20 +11,20 @@ int main()
 	using namespace std::chrono;
 	
 	// Path to input and output file
-	string iFile("/home/stefano/Desktop/mesh-simplification/mesh/cow.inp");
+	string iFile("/home/stefano/Desktop/mesh-simplification/mesh/bunny.inp");
 	string oFile("/home/stefano/Desktop/mesh-simplification/mesh/cow_1160_033_033_033_greedy_30.inp");
 	
 	// Desired number of nodes
-	UInt numNodesMax(1160);
+	UInt numNodesMax(20000);
 	
 	// Simplificate!
 	#ifdef NDEBUG
 	high_resolution_clock::time_point start = high_resolution_clock::now();
 	#endif
 	
-	simplification<Triangle, MeshType::DATA, DataGeo> simplifier(iFile);
-	//simplifier.simplificate(numNodesMax, true, oFile);
-	simplifier.simplificate_greedy(numNodesMax, 30, true, oFile);
+	simplification<Triangle, MeshType::GEO, OnlyGeo<MeshType::GEO>> simplifier(iFile);
+	simplifier.simplificate(numNodesMax, false);
+	//simplifier.simplificate_greedy(numNodesMax, 30, true, oFile);
 	
 	#ifdef NDEBUG
 	high_resolution_clock::time_point stop = high_resolution_clock::now();
